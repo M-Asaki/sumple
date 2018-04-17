fde4e9da6ac4a250d997b5f4265a18e142265cd6
A-Mizuno <aoba0106koyo1012@gmail.com>
2018-04-17 10:29:49 +0900

    pictgram 1.0

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -4,13 +4,17 @@ class TopicsController < ApplicationController
   end
 
   def create
-    @topic = current_user.topics.new(topic_params)
+    @topic = current_user.Topic.new(topic_params)
       if @topic.save
         redirect_to topics_path, notice: "投稿に成功しました"
       else
         flash.now[:alert] = "投稿に失敗しました"
         render :new
       end
+  end
+
+  def index
+    @topics = Topic.all
   end
 
   private
